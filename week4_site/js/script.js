fbbc90124729b4f7e506e2d7f5dcadf92f366675
Wesley Chou <wesleychou9851@gmail.com>
2023-01-07 20:44:18 -0800

    testing

--- week4_site/js/script.js
+++ week4_site/js/script.js
@@ -1,1 +1,11 @@
-var x = "HEyy"
+var names = ["wes", "bob", "joe"];
+
+names.special = "x12";
+names[4] = "ash";
+
+console.log(names[3]);
+
+for (var name in names){
+    console.log(name);
+    console.log("Hello " + names[name]);
+}
