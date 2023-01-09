d8d37b003334238a29cfec03bcc35c2cf744192e
Wesley Chou <wesleychou9851@gmail.com>
2023-01-08 20:18:01 -0800

    added new function to load main content

--- david_chus_china_bistro/js/script.js
+++ david_chus_china_bistro/js/script.js
@@ -7,3 +7,32 @@ $(function(){
         }
     });
 });
+
+(function(global){
+    var dc = {};
+    var homeHtml = "snippets/home-snippet.html";
+
+    // convinience function for inserting innerHTML for 'select'
+    var insertHtml = function(selector, html){
+        document.querySelector(selector).innerHTML = html;
+    };
+
+    // show loading icon inside element identified by 'selector'
+    var showLoading = function(selector){
+        var html = "<div class='text-center'><img src='images/ajax-loader.gif'></div>";
+        insertHtml(selector, html);
+    };
+
+    document.addEventListener("DOMContentLoaded", function(event){
+        showLoading("#main-content");
+        $ajaxUtils.sendGetRequest(
+            homeHtml,
+            function(responseText){
+                document.querySelector("#main-content").innerHTML = responseText;
+            },
+            false
+        );
+    });
+
+    global.$dc = dc;
+})(window);
